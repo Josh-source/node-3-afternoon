3454e8f6bfe5a6f8bdc1fa5c877391fe91cea6d0
Joshua Riley <Joshy@Faiths-MacBook-Pro.local>
2019-10-14 15:22:06 -0500

    hopefully fixed

--- index.js
+++ index.js
@@ -1,29 +1,27 @@
+const express = require('express')
+const massive = require('massive')
+const products_controller =require('./products_controller')
 require('dotenv').config();
-const express = require('express');
-const massive = require('massive');
-const products_controller = require("./products_controller");
 
-const app = express();
+const app = express()
 
-
-const{SERVER_PORT, CONNECTION_STRING} = process.env;
+const {SERVER_PORT, CONNECTION_STRING}=process.env
 
 massive(CONNECTION_STRING)
-    .then(dbInstance => {
-        app.set("db", dbInstance);
-        console.log("db connected");
-    })
-    .catch(err => console.log(err));
-
-
-app.use(express.json());
-
-app.post('/api/products', products_controller.create);
-app.get('/api/products', products_controller.getAll);
-app.get('/api/products/:id', products_controller.getOne);
-app.put('/api/products/:id', products_controller.update);
-app.delete('/api/products/:id', products_controller.deleteOne);
-
-app.listen(SERVER_PORT, () => {
-    console.log(`Server Listening on port ${SERVER_PORT}.`);
+.then(dbInstance =>{
+    app.set('db',dbInstance)
+    console.log('Database Connected')
 })
+.catch(err =>console.log(err))
+app.use(express.json())
+
+app.post('/api/products',products_controller.create)
+app.get('/api/products',products_controller.getAll)
+app.get('/api/products/:id',products_controller.getOne)
+app.put('/api/products/:id',products_controller.update)
+app.delete('/api/products/:id',products_controller.delete)
+
+
+
+
+app.listen(SERVER_PORT,()=>console.log(`server listening on port ${SERVER_PORT}.`))
